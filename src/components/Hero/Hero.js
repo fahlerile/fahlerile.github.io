3150704cf4f10d0a42537b606399f692b1b69450
kitro <kitrofimov39@gmail.com>
2023-05-27 08:52:44 +0500

    decided to put arrow and about

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,12 +4,6 @@ import './Hero.css';
 import avatar from '../../images/avatar.jpg';
 
 function Hero() {
-  function handleClick() {
-    setShow(prev => !prev);
-  }
-
-  const [show, setShow] = React.useState(false);
-
   return (
       <div className='hero'>
         <div className='hero__container'>
@@ -17,14 +11,11 @@ function Hero() {
             <div className='hero__container__text'>
                 <h1>Kirill <span className='pink'>(fahlerile)</span> Trofimov</h1>
                 <h2>ML/DL enthusiast</h2>
-                <span className="material-symbols-outlined" onClick={handleClick}>
-                  expand_more
-                </span>
-                <div className={`hero__container__text__conditional ${show ? null : 'hidden'}`}>
-                  My name is Patrick Bateman. I'm 27 years old. I believe in taking care of myself, and a balanced diet and a rigorous exercise routine.
-                </div>
             </div>
         </div>
+        <span class="material-symbols-outlined">
+          arrow_downward
+        </span>
       </div>
   );
 }
