908f606adab6070799f67fc9f55b3a175231f59a
kitro <kitrofimov39@gmail.com>
2023-05-28 17:25:57 +0500

    Added responsive css, fixed footer in popups, changed cursor property

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,8 @@ import './Hero.css';
 function Hero() {
   return (
     <div className="hero">
-        <h1>My name is Kirill</h1>
-        <p>I am 15 years old and I am interested in programming and machine learning! On this page you can check out my best projects.</p>
+        <h1>Some heading</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
   );
 }
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,9 +5,9 @@ function Project(props) {
   const {name, date, img} = props.data;
 
   return (
-    <div className="project" style={{gridArea: props.position.join(' / ')}} onClick={props.onClick}>
+    <div className="project" onClick={props.onClick}>
       <div className='project__imageBg' >
-         <img src={require(`../../../public/images/${img}`)} alt='project'/>
+         <img src={require(`../../../public/assets/${img}`)} alt='project'/>
       </div>
       <h2 className="title">{name}</h2>
       <h3 className="subtitle gray">{date}</h3>
